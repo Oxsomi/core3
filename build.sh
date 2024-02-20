4fb8e347fac6cf7207f330d19882a0f165bf6aa7
Nielsbishere <n@osomi.net>
2024-02-20 19:09:19 +0100

    Added Vulkan swapchain extension name for osx/ios. OxC3 platforms now properly sets C variables for iOS, Android and OSX. Added bash version of build.bat to be compatible with unix builds.

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+usage() {
+	echo Usage: build [Build type: Debug/Release] [Enable SIMD: On/Off] [Force float fallback: On/Off]
+	exit 1
+}
+
+if [[ "$1" != @(Release|Debug) ]]; then usage; fi
+if [[ "$2" != @(On|Off) ]]; then usage; fi
+if [[ "$3" != @(On|Off) ]]; then usage; fi
+
+# Build normal exes
+
+echo -- Building tests...
+
+mkdir builds
+cd builds
+cmake -DCMAKE_BUILD_TYPE=$1 .. -DEnableSIMD=$2 -DForceFloatFallback=$3
+cmake --build . -j 8 --config $1
+cd ../
+
+rem Run unit test
+
+./builds/bin/$1/OxC3_test.exe
