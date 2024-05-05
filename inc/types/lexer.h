6d5b5a83a48063517a1f780c26b70d6f2c554051
Nielsbishere <n@osomi.net>
2024-05-05 11:32:16 +0200

    Fixed C++ support and moved core_bindings includes to OxC3 types. Also fixed the license

--- inc/types/lexer.h
+++ inc/types/lexer.h
@@ -1,18 +1,18 @@
-/* OxC3_bindings (Oxsomi core 3 / bindings), for generating OxC3 bindings for different languages (such as C#).
+/* OxC3(Oxsomi core 3), a general framework and toolset for cross-platform applications.
 *  Copyright (C) 2023 Oxsomi / Nielsbishere (Niels Brunekreef)
-*  
+*
 *  This program is free software: you can redistribute it and/or modify
 *  it under the terms of the GNU General Public License as published by
 *  the Free Software Foundation, either version 3 of the License, or
 *  (at your option) any later version.
-*  
+*
 *  This program is distributed in the hope that it will be useful,
 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 *  GNU General Public License for more details.
-*  
+*
 *  You should have received a copy of the GNU General Public License
-*  along with this program. If not, see https://github.com/Oxsomi/rt_core/blob/main/LICENSE.
+*  along with this program. If not, see https://github.com/Oxsomi/core3/blob/main/LICENSE.
 *  Be aware that GPL3 requires closed source products to be GPL3 too if released to the public.
 *  To prevent this a separate license will have to be requested at contact@osomi.net for a premium;
 *  This is called dual licensing.
@@ -20,6 +20,10 @@
 
 #pragma once
 #include "types/list.h"
+
+#ifdef __cplusplus
+	extern "C" {
+#endif
 
 typedef struct CharString CharString;
 
@@ -83,3 +87,7 @@ typedef struct Lexer {
 
 Error Lexer_create(CharString source, Lexer *lexer);
 Bool Lexer_free(Lexer *lexer);
+
+#ifdef __cplusplus
+	}
+#endif
