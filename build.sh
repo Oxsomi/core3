f2eda98ef7430d6995aeca26c817c17f636ee479
Nielsbishere <n@osomi.net>
2024-08-12 22:14:11 +0200

    Added error reporting to build.sh for OxC3_test, test.sh or conan failing.

--- build.sh
+++ build.sh
@@ -9,14 +9,34 @@ if [ "$1" != Release ] && [ "$1" != Debug ]; then usage; fi
 if [ "$2" != True ] && [ "$2" != False ]; then usage; fi
 if [ "$3" != True ] && [ "$3" != False ]; then usage; fi
 
-conan create external/dxc -s build_type=$1 --build=missing
-conan build . -s build_type=$1 -o enableSIMD=$2 -o enableTests=$3 ${@:4}
+RED='\033[0;31m'
+NC='\033[0m'
+
+if ! conan create external/dxc -s build_type=$1 --build=missing; then
+	printf "${RED}-- Conan create DXC failed${NC}\n"
+	exit 1
+fi
+
+if ! conan build . -s build_type=$1 -o enableSIMD=$2 -o enableTests=$3 ${@:4}; then
+	printf "${RED}-- Conan build failed${NC}\n"
+	exit 1
+fi
 
 # Run tests
 
 if [ "$3" == True ]; then
+
 	cd build/$1/bin
-	./OxC3_test
-	bash ../../../tools/test.sh
+
+	if ! ./OxC3_test ; then 
+		printf "${RED}-- OxC3_test failed${NC}\n"
+		exit 1
+	fi
+
+	if ! bash ../../../tools/test.sh ; then
+		printf "${RED}-- test.sh failed${NC}\n"
+		exit 1
+	fi
+
 	cd ../../..
 fi
