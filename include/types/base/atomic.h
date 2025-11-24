8033f2bd04f131321d5b22e91213b063e22cf9d3
Nielsbishere <n@osomi.net>
2025-11-24 19:22:38 +0100

    Moved atomic implementation into atomic.h with inline

--- include/types/base/atomic.h
+++ include/types/base/atomic.h
@@ -19,7 +19,7 @@
 */
 
 #pragma once
-#include "types/base/types.h"
+#include "types/base/constants.h"
 #include "types/base/platform_types.h"
 
 #if _PLATFORM_TYPE != PLATFORM_WINDOWS
@@ -30,6 +30,7 @@
 		#define _Atomic(X) std::atomic<X>
 	#endif
 #else
+	#include <intrin.h>
 	#define _Atomic(T) T
 #endif
 
@@ -45,22 +46,119 @@ typedef struct AtomicI64 {
 
 //Bitwise
 
-impl I64 AtomicI64_xor(AtomicI64 *ptr, I64 value);
-impl I64 AtomicI64_and(AtomicI64 *ptr, I64 value);
-impl I64 AtomicI64_or(AtomicI64 *ptr, I64 value);
-impl I64 AtomicI64_load(AtomicI64 *ptr);
-impl I64 AtomicI64_store(AtomicI64 *ptr, I64 value);
-impl I64 AtomicI64_cmpStore(AtomicI64 *ptr, I64 compare, I64 value);		//If value in atomic is compare set to value
+impl static inline I64 AtomicI64_xor(AtomicI64 *ptr, I64 value);
+impl static inline I64 AtomicI64_and(AtomicI64 *ptr, I64 value);
+impl static inline I64 AtomicI64_or(AtomicI64 *ptr, I64 value);
+impl static inline I64 AtomicI64_load(AtomicI64 *ptr);
+impl static inline I64 AtomicI64_store(AtomicI64 *ptr, I64 value);
+impl static inline I64 AtomicI64_cmpStore(AtomicI64 *ptr, I64 compare, I64 value);		//If value in atomic is compare set to value
 
 //Return state of atomic before adding value.
 
-impl I64 AtomicI64_add(AtomicI64 *ptr, I64 value);
-impl I64 AtomicI64_sub(AtomicI64 *ptr, I64 value);		//I64 = I64_MIN becomes NO-OP (only fetches, doesn't increment)
+impl static inline I64 AtomicI64_add(AtomicI64 *ptr, I64 value);
+impl static inline I64 AtomicI64_sub(AtomicI64 *ptr, I64 value);		//I64 = I64_MIN becomes NO-OP (only fetches, doesn't increment)
 
 //Return state of atomic after adding value.
 
-impl I64 AtomicI64_inc(AtomicI64 *ptr);
-impl I64 AtomicI64_dec(AtomicI64 *ptr);
+impl static inline I64 AtomicI64_inc(AtomicI64 *ptr);
+impl static inline I64 AtomicI64_dec(AtomicI64 *ptr);
+
+//Implementation
+
+#if _PLATFORM_TYPE == PLATFORM_WINDOWS
+
+	static inline I64 AtomicI64_and(AtomicI64 *ptr, I64 value) {
+		return _InterlockedAnd64(&ptr->atomic, value);
+	}
+
+	static inline I64 AtomicI64_xor(AtomicI64 *ptr, I64 value) {
+		return _InterlockedXor64(&ptr->atomic, value);
+	}
+
+	static inline I64 AtomicI64_or(AtomicI64 *ptr, I64 value) {
+		return _InterlockedOr64(&ptr->atomic, value);
+	}
+
+	static inline I64 AtomicI64_load(AtomicI64 *ptr) {
+		return AtomicI64_add(ptr, 0);
+	}
+
+	static inline I64 AtomicI64_add(AtomicI64 *ptr, I64 value) {
+		return _InterlockedExchangeAdd64(&ptr->atomic, value);
+	}
+
+	static inline I64 AtomicI64_store(AtomicI64 *ptr, I64 value) {
+		return _InterlockedExchange64(&ptr->atomic, value);
+	}
+
+	static inline I64 AtomicI64_cmpStore(AtomicI64 *ptr, I64 compare, I64 value) {
+		return _InterlockedCompareExchange64(&ptr->atomic, value, compare);
+	}
+
+	static inline I64 AtomicI64_sub(AtomicI64 *ptr, I64 value) {
+
+		if(value == I64_MIN)
+			value = 0;
+
+		return AtomicI64_add(ptr, -value);
+	}
+
+	static inline I64 AtomicI64_inc(AtomicI64 *ptr) {
+		return _InterlockedIncrement64(&ptr->atomic);
+	}
+
+	static inline I64 AtomicI64_dec(AtomicI64 *ptr) {
+		return _InterlockedDecrement64(&ptr->atomic);
+	}
+
+#else
+
+	static inline I64 AtomicI64_and(AtomicI64 *ptr, I64 value) {
+		return atomic_fetch_and(&ptr->atomic, value);
+	}
+
+	static inline I64 AtomicI64_xor(AtomicI64 *ptr, I64 value) {
+		return atomic_fetch_xor(&ptr->atomic, value);
+	}
+
+	static inline I64 AtomicI64_or(AtomicI64 *ptr, I64 value) {
+		return atomic_fetch_or(&ptr->atomic, value);
+	}
+
+	static inline I64 AtomicI64_load(AtomicI64 *ptr) {
+		return AtomicI64_add(ptr, 0);
+	}
+
+	static inline I64 AtomicI64_add(AtomicI64 *ptr, I64 value) {
+		return atomic_fetch_add(&ptr->atomic, value);
+	}
+
+	static inline I64 AtomicI64_store(AtomicI64 *ptr, I64 value) {
+		return atomic_exchange(&ptr->atomic, value);
+	}
+
+	static inline I64 AtomicI64_cmpStore(AtomicI64 *ptr, I64 compare, I64 value) {
+		atomic_compare_exchange_strong(&ptr->atomic, &compare, value);
+		return compare;
+	}
+
+	static inline I64 AtomicI64_sub(AtomicI64 *ptr, I64 value) {
+
+		if(value == I64_MIN)
+			value = 0;
+
+		return AtomicI64_add(ptr, -value);
+	}
+
+	static inline I64 AtomicI64_inc(AtomicI64 *ptr) {
+		return AtomicI64_add(ptr, 1) + 1;	//add returns old value, so correct for inc
+	}
+
+	static inline I64 AtomicI64_dec(AtomicI64 *ptr) {
+		return AtomicI64_sub(ptr, 1) - 1;	//sub returns old value, so correct for dec
+	}
+
+#endif
 
 #ifdef __cplusplus
 	}
