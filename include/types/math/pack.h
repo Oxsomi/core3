22b6774c4696d1730c541a5348787a252bcddff7
Nielsbishere <n@osomi.net>
2026-01-10 18:06:12 +0100

    Made all pack.h functions static inline

--- include/types/math/pack.h
+++ include/types/math/pack.h
@@ -20,6 +20,7 @@
 
 #pragma once
 #include "types/math/quat.h"
+#include "types/base/constants.h"
 
 #ifdef __cplusplus
 	extern "C" {
@@ -27,17 +28,97 @@
 
 //U64 packing
 
-U64 U64_pack21x3(U32 x, U32 y, U32 z);
-U32 U64_unpack21x3(U64 packed, U8 off);
-Bool U64_setPacked21x3(U64 *packed, U8 off, U32 v);
+static inline U64 U64_pack21x3(U32 x, U32 y, U32 z) {
 
-Bool U64_pack20x3u4(U64 *dst, U32 x, U32 y, U32 z, U8 u4);
-U32 U64_unpack20x3u4(U64 packed, U8 off);
-Bool U64_setPacked20x3u4(U64 *packed, U8 off, U32 v);
+	if ((x >> 21) || (y >> 21) || (z >> 21))
+		return U64_MAX;									//Returns U64_MAX on invalid
 
-#define GET_BIT_OP(T)								\
-Bool T##_getBit(T packed, U8 off);					\
-Bool T##_setBit(T *packed, U8 off, Bool b)
+	return x | ((U64)y << 21) | ((U64)z << 42);
+}
+
+static inline Bool U64_pack20x3u4(U64 *dst, U32 x, U32 y, U32 z, U8 u4) {
+
+	if (!dst || (x >> 20) || (y >> 20) || (z >> 20) || (u4 >> 4))
+		return false;
+
+	*dst = x | ((U64)y << 20) | ((U64)z << 40) | ((U64)u4 << 60);
+	return true;
+}
+
+static inline U32 U64_unpack21x3(U64 packed, U8 off) {
+
+	if ((packed >> 63) || (off >= 3))
+		return U32_MAX;									//Returns U32_MAX on invalid
+
+	return (U32)((packed >> (21 * off)) & ((1 << 21) - 1));
+}
+
+static inline U32 U64_unpack20x3u4(U64 packed, U8 off) {
+
+	if (off > 3)
+		return U32_MAX;									//Returns U32_MAX on invalid
+
+	if (off == 3)
+		return packed >> 60;
+
+	return (U32)((packed >> (20 * off)) & ((1 << 20) - 1));
+}
+
+static inline Bool U64_setPacked20x3u4(U64 *packed, U8 off, U32 v) {
+
+	if (off == 3) {
+
+		if(!packed || v >> 4)
+			return false;
+
+		*packed &= ~((U64)0xF << 60);
+		*packed |= (U64)v << 60;
+		return true;
+	}
+
+	if (v >> 20 || off > 3 || !packed)
+		return false;
+
+	off *= 20;
+	*packed &= ~((U64)((1 << 20) - 1) << off);		//Reset bits
+	*packed |= (U64)v << off;						//Set bits
+	return true;
+}
+
+static inline Bool U64_setPacked21x3(U64 *packed, U8 off, U32 v) {
+
+	if (v >> 21 || off >= 3 || !packed)
+		return false;
+
+	off *= 21;
+	*packed &= ~((U64)((1 << 21) - 1) << off);		//Reset bits
+	*packed |= (U64)v << off;						//Set bits
+	return true;
+}
+
+#define GET_BIT_OP(T)										\
+static inline Bool T##_getBit(T packed, U8 off) {			\
+															\
+	if (off >= sizeof(T) * 8)								\
+		return false;										\
+															\
+	return (packed >> off) & 1;								\
+}															\
+															\
+static inline Bool T##_setBit(T *packed, U8 off, Bool b) {	\
+															\
+	if (off >= sizeof(T) * 8)								\
+		return false;										\
+															\
+	T shift = (T)1 << off;									\
+															\
+	if (b)													\
+		*packed |= shift;									\
+															\
+	else *packed &= ~shift;									\
+															\
+	return true;											\
+}
 
 GET_BIT_OP(U64);
 GET_BIT_OP(U32);
@@ -50,9 +131,29 @@ typedef struct QuatS16 {
 	I16 arr[4];
 } QuatS16;
 
-QuatF32 QuatS16_unpack(QuatS16 q);
-I32x4 QuatS16_unpackI32(QuatS16 q);
-QuatS16 QuatF32_pack(QuatF32 q);
+static inline I32x4 QuatS16_unpackI32(QuatS16 q) {
+	return I32x4_create4(q.arr[0], q.arr[1], q.arr[2], q.arr[3]);
+}
+
+static inline QuatF32 QuatS16_unpack(QuatS16 q) {
+	const F32x4 v = F32x4_create4(q.arr[0], q.arr[1], q.arr[2], q.arr[3]);
+	return F32x4_div(v, F32x4_xxxx4(I16_MAX));
+}
+
+static inline QuatS16 QuatF32_pack(QuatF32 q) {
+
+	q = QuatF32_normalize(q);
+	const F32x4 asI16 = F32x4_mul(q, F32x4_xxxx4(I16_MAX));
+
+	return (QuatS16) {
+		{
+			(I16)F32x4_x(asI16),
+			(I16)F32x4_y(asI16),
+			(I16)F32x4_z(asI16),
+			(I16)F32x4_w(asI16)
+		}
+	};
+}
 
 #ifdef __cplusplus
 	}
