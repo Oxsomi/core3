c5c0df3eaab4d5f3369015c72f92744db86eaf55
Nielsbishere <n@osomi.net>
2026-02-06 01:18:33 +0100

    Update DXC to fix OSX compile issues

--- packages/dxc/conanfile.py
+++ packages/dxc/conanfile.py
@@ -9,7 +9,7 @@ required_conan_version = ">=2.0"
 class dxc(ConanFile):
 
 	name = "dxc"
-	version = "2025.11.17"
+	version = "2026.02.05.09"
 
 	# Optional metadata
 	license = "LLVM Release License"
